1306164d6a52afe4e98d624672d5ff125a09fa98
grimmi <arne.lehmann@gmx.de>
2013-08-28 20:16:42 +0200

    String Reverse Aufgabe (komplett)

---
+++ stringReverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter string: ");
+                string inputString = Console.ReadLine();
+                if (!inputString.Equals(""))
+                {
+                    string reverseString = "";
+                    for (int i = inputString.Length-1; i >= 0; i--)
+                    {
+                        reverseString += inputString[i];
+                    }
+                    Console.WriteLine("Reverse String: {0}", reverseString);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Empty String!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
