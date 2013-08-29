bd5caad12e6f0c3fcf683f903eaa23181a9d9072
grimmi <arne.lehmann@gmx.de>
2013-08-29 14:45:28 +0200

    unnötige importe entfernt

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
  * Fibonacci Sequence - Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
