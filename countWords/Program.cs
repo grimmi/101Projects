7ffb509f5d05b6fc250211719fa64fe23e334a3b
grimmi <arne.lehmann@gmx.de>
2013-08-31 10:44:09 +0200

    unnötige imports entfernt - wordCount

--- countWords/Program.cs
+++ countWords/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
  * Count Words in a String - Counts the number of individual words in a string. For added complexity read these strings in from a text file and generate a summary.
