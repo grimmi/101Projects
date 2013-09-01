505b90d0574f77720bccef8ed5aef14ccd23997f
grimmi <arne.lehmann@gmx.de>
2013-09-01 15:02:54 +0200

    airline reservation begonnen

---
+++ airline reservation/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace airline_reservation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flightList = new System.Windows.Forms.ListView();
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.start = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.destination = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // flightList
+            // 
+            this.flightList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.id,
+            this.start,
+            this.destination});
+            this.flightList.Location = new System.Drawing.Point(12, 12);
+            this.flightList.Name = "flightList";
+            this.flightList.Size = new System.Drawing.Size(271, 495);
+            this.flightList.TabIndex = 0;
+            this.flightList.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 519);
+            this.Controls.Add(this.flightList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView flightList;
+        public System.Windows.Forms.ColumnHeader id;
+        public System.Windows.Forms.ColumnHeader start;
+        public System.Windows.Forms.ColumnHeader destination;
+    }
+}
+
